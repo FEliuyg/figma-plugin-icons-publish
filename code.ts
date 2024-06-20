41faf8142a949666ecd8d28a5ef71d39d36f45e3
xiaoyao <15558110117@163.com>
2024-06-20 14:31:28 +0800

    feat: init figma type project

--- code.ts
+++ code.ts
@@ -1,5 +1,5 @@
-// This plugin will open a tab that indicates that it will monitor the current
-// selection on the page. It cannot change the document itself.
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
 // This file holds the main code for plugins. Code in this file has access to
 // the *figma document* via the figma global object.
@@ -9,7 +9,26 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// This monitors the selection changes and posts the selection to the UI
-figma.on('selectionchange', () => {
-  figma.ui.postMessage(figma.currentPage.selection)
-});
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-rectangles') {
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
